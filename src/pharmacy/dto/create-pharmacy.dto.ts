536f94651d4639cfbdac7667824cedf5509f32d0
Josè Cossa <JoseBanze1@gmail.com>
2025-06-18 23:13:34 +0200

    agora ja o sistema ja da mengens bem mais claras para lidar com erros

--- src/pharmacy/dto/create-pharmacy.dto.ts
+++ src/pharmacy/dto/create-pharmacy.dto.ts
@@ -1,16 +1,19 @@
-import { IsString, IsOptional, IsPhoneNumber } from 'class-validator';
+import { IsString, IsOptional, IsPhoneNumber,IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePharmacyDto {
   @ApiProperty({ example: 'Farmácia Central' })
+  @IsNotEmpty({ message: 'O nome é obrigatório.' })
   @IsString()
   name: string;
 
   @ApiProperty({ example: 'Av. Eduardo Mondlane, Maputo' })
+  @IsNotEmpty({ message: 'O endereço é obrigatório.' })
   @IsString()
   address: string;
 
   @ApiProperty({ example: '+258841234567', required: false })
+  @IsString({ message: 'O telefone deve ser uma string.' })
   @IsOptional()
   @IsPhoneNumber('MZ')
   phone?: string;
